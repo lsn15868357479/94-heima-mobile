38aff361f01d141c879faadb700de9f9321261fd
lsn15868357479 <2658523464@qq.com>
2020-03-17 20:28:30 +0800

    登录-基础布局

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,6 +3,20 @@
     <!-- 导航 -->
     <van-nav-bar title="登录" left-arrow @click-left="$router.back()">
     </van-nav-bar>
+    <!-- 登录页面 -->
+    <van-cell-group>
+      <!-- 登录手机号 -->
+      <van-field label="手机号" placeholder="请输入手机号"  error-message="手机号格式错误">
+      </van-field>
+      <!-- 获取验证码 -->
+      <van-field label="验证码" placeholder="请输入验证码"  error-message="验证码错误">
+         <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <!-- 登录按钮 -->
+  <div class="login_box">
+    <van-button round type="info" block>登录</van-button>
+  </div>
   </div>
 </template>
 
@@ -12,6 +26,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+   .login_box {
+     padding: 20px;
+   }
 </style>
