1f452c3f4db467b06b83614539d26ac1469af1b7
lsn15868357479 <2658523464@qq.com>
2020-03-18 20:19:16 +0800

    使用tabs组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <div class="home">
-        首页
+  <div class="container">
+    <!-- 放置tabs组件 -->
+    <van-tabs>
+       <!-- 子标签 title为当前显示内容-->
+       <van-tab :title="`标签${item}`" v-for="item in 10" :key="item">
+       <div class="scroll-wrapper">
+         <van-cell-group>
+           <van-cell title="标题" value="内容" v-for="item in 20" :key="item"></van-cell>
+         </van-cell-group>
+       </div>
+       </van-tab>
+    </van-tabs>
+    <!-- 放置编辑频道图标 -->
+    <span class="bar_btn">
+      <!-- 放入图标 -->
+      <van-icon name="wap-nav" />
+    </span>
   </div>
 </template>
 
@@ -9,7 +24,63 @@
 
 export default {
   name: 'Home',
-  components: {
-  }
+  components: {}
 }
 </script>
+<style lang="less" scoped>
+.van-tabs {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /deep/ .van-tabs__wrap {
+    height: 36px;
+    padding-right: 36px;
+    .van-tab {
+      line-height: 36px;
+    }
+    .van-tabs__line {
+      background-color: #3296fa;
+      height: 2px;
+    }
+  }
+  /deep/ .van-tabs__content{
+    flex: 1;
+    overflow: hidden;
+  }
+  /deep/ .van-tab__pane{
+    height: 100%;
+    .scroll-wrapper{
+      height: 100%;
+      overflow-y: auto;
+    }
+  }
+}
+.bar_btn {
+  width: 36px;
+  height: 35px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  &::before {
+    content: "";
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    z-index: 999;
+    box-shadow: 0 0 10px #999;
+    transform: scale(1, 0.6);
+  }
+  .van-icon-wap-nav {
+    width: 100%;
+    height: 100%;
+    background: #fff;
+    text-align: center;
+    line-height: 35px;
+    position: relative;
+    z-index: 1000;
+    &::before {
+      font-size: 20px;
+    }
+  }
+}
+</style>
