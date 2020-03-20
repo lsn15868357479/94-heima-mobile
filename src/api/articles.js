08d3386c516c296a7b989450b3369a9159fb238c
lsn15868357479 <2658523464@qq.com>
2020-03-20 17:03:21 +0800

    封装获取文章列表api

---
+++ src/api/articles.js
@@ -0,0 +1,12 @@
+// 处理文章模块的请求数据
+import request from '@/utils/request'
+
+// 获取文章数据
+export function getArticles (params) {
+  return request({
+    url: 'http://ttapi.research.itcast.cn/app/v1_1/articles',
+    params: {
+      with_top: 1, ...params// 合并数据
+    }
+  })
+}
